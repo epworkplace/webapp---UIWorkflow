43e725a29c67149e3a53c5617b1611df362e1cad
Priyam-EP <pchakraborty@edit-place.com>
2018-02-22 17:29:46 +0530

    major changes in BO offers single

--- lbd/js/custom.js
+++ lbd/js/custom.js
@@ -538,7 +538,7 @@ $(document).ready(function(){
 		height: "300",
 		plugins: [
 		"advlist autolink lists link image charmap print preview anchor",
-		"searchreplace visualblocks code fullscreen",
+		"searchreplace visualblocks fullscreen",
 		"insertdatetime media table contextmenu paste imagetools wordcount"
 		],
 		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
